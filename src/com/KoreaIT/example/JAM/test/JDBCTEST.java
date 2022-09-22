71d5ec6d3ee77539179c112b4e95e7050f80c8b7
ksscok1 <ksscok2@gmail.com>
2022-09-22 16:59:52 +0900

    github_exercise

--- src/com/KoreaIT/example/JAM/test/JDBCTEST.java
+++ src/com/KoreaIT/example/JAM/test/JDBCTEST.java
@@ -24,7 +24,7 @@ public class JDBCTEST {
 				if (conn != null && !conn.isClosed()) {
 					conn.close();
 				}
-			} catch (SQLException e) { //asdfasdf
+			} catch (SQLException e) { //asdfasdf asdfasdf
 				e.printStackTrace();
 			}
 		}
